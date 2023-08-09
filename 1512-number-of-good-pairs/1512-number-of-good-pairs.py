0a129c327f457bf09b2a22e976d844f8710d8bca
Luis Rodriguez <101071780+pmentor@users.noreply.github.com>
2023-08-09 11:28:00 -0400

    Time: 45 ms (70.83%), Space: 16.2 MB (67.27%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,9 +1,12 @@
+# O(n) solution using hash_map
+# Assuming that the number of pairs for n identical numbers is n*(n-1)/2
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         res: int = 0
-        for i, num in enumerate(nums):
-            for j in range(i + 1, len(nums)):
-                if nums[i] == nums[j]:
-                    res += 1
-
-        return res
+        # Create hashmap to count number of repetitions
+        hash_map: dict[int, int] = dict()
+        for num in nums:
+            # Increase num counter in the hash_map using 0 as default value
+            hash_map[num] = hash_map.get(num, 0) + 1
+        # Sum hash_map using n*(n-1)/2 as number of pairs for identical numbers
+        return sum(map(lambda x: int(x * (x - 1) / 2), hash_map.values()))
