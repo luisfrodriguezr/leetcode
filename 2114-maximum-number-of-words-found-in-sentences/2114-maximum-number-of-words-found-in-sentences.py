e8e8d4ca5bfbabe4badb6a6d1265837ce522c64c
Luis Rodriguez <101071780+pmentor@users.noreply.github.com>
2023-08-19 21:21:05 -0400

    Time: 47 ms (86.43%), Space: 16.3 MB (89.89%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.py
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.py
@@ -1,6 +1,5 @@
 class Solution:
     def mostWordsFound(self, sentences: List[str]) -> int:
-        # Return the max number of words in a list of sentences
-        # A sentence has 'nspaces+1' words
-        space: str = ' '
-        return max(sentence.count(space) + 1 for sentence in sentences)
+        #Return the max number of words in a list of sentences
+        #A sentence has 'nspaces+1' number of words
+        return max(sentence.count(' ') + 1 for sentence in sentences)
