d12188e979e34ac1ca8019268662c6099bf9ef48
Luis Rodriguez <101071780+pmentor@users.noreply.github.com>
2023-12-20 12:36:28 -0400

    Time: 37 ms (75.35%), Space: 17.3 MB (22.29%) - LeetHub

---
+++ 1037-valid-boomerang/1037-valid-boomerang.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isBoomerang(self, points: List[List[int]]) -> bool:
+        area = (
+            points[0][0] * (points[1][1] - points[2][1])
+            + points[1][0] * (points[2][1] - points[0][1])
+            + points[2][0] * (points[0][1] - points[1][1])
+        ) / 2
+        if area:
+            return True
+        return False
