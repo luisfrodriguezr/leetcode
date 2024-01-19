3172dd3efcad3a69717e12bcf987606f6d2ea1b2
Luis Rodriguez <101071780+luisfrodriguezr@users.noreply.github.com>
2024-01-18 20:26:35 -0400

    Time: 138 ms (98.16%), Space: 17.3 MB (26.21%) - LeetHub

--- 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.py
+++ 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.py
@@ -1,17 +1,12 @@
 class Solution:
   def countBeautifulPairs(self, nums: List[int]) -> int:
-    def coprime(x, y):
-      for i in range(2, min(x, y) + 1):
-        if x % i == 0 and y % i == 0:
-          return False
-      return True
-
     ans = 0
+    digits = [0] * 10
     for i in range(len(nums)):
-      for j in range(i + 1, len(nums)):
-        x = str(nums[i])[0]
-        y = str(nums[j])[-1]
-        if coprime(int(x), int(y)):
-          ans += 1
-
+        x = str(nums[i])[-1]
+        f = str(nums[i])[0]
+        for y in range(1, len(digits)):
+          if digits[y]:
+            if (math.gcd(y, int(x)) == 1): ans += digits[y]
+        digits[int(f)] += 1
     return ans
