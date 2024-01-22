dcf98ab9860d14c56f7987d20f317334bdd2d834
Luis Rodriguez <101071780+luisfrodriguezr@users.noreply.github.com>
2024-01-22 10:13:10 -0400

    Time: 35 ms (78.26%), Space: 16.4 MB (61.76%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    def __init__(self):
+        self.memo = {}
+    
+    def rob(self, nums: List[int]) -> int:
+        
+        self.memo = {}
+        
+        return self.robFrom(0, nums)
+    
+    def robFrom(self, i, nums):
+        
+        if i >= len(nums):
+            return 0
+        if i in self.memo:
+            return self.memo[i]
+        ans = max(self.robFrom(i + 1, nums), self.robFrom(i + 2, nums) + nums[i])
+        self.memo[i] = ans
+        return ans
