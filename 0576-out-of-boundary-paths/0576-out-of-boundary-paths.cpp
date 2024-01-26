0a22356262e5b66636e3f9065faa2d80f8735532
Luis Rodriguez <101071780+luisfrodriguezr@users.noreply.github.com>
2024-01-26 19:20:54 -0400

    Time: 9 ms (45.83%), Space: 10.4 MB (24.21%) - LeetHub

---
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int M = 1000000007;
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        vector<vector<vector<int>>> memo(m, vector<vector<int>>(n, vector<int>(maxMove + 1, -1)));
+        return findPaths(m, n, maxMove, startRow, startColumn, memo);
+    }
+    int findPaths(int m, int n, int N, int i, int j, vector<vector<vector<int>>>& memo) {
+        if (i == m || j == n || i < 0 || j < 0)
+            return 1;
+        if (N == 0)
+            return 0;
+        if (memo[i][j][N] >= 0)
+            return memo[i][j][N];
+        memo[i][j][N] = ((findPaths(m, n, N - 1, i - 1, j, memo) + findPaths(m, n, N - 1, i + 1, j, memo)) % M +
+                         (findPaths(m, n, N - 1, i, j - 1, memo) + findPaths(m, n, N - 1, i, j + 1, memo)) % M) % M;
+        return memo[i][j][N];
+    }
+};
