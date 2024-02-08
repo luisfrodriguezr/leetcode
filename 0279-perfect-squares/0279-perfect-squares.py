717232775ce74a7349280b22afd7ddb54daccd5d
Luis Rodriguez <101071780+luisfrodriguezr@users.noreply.github.com>
2024-02-08 18:33:30 -0400

    Time: 3192 ms (31.34%), Space: 16.6 MB (81.95%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,20 +1,11 @@
 class Solution:
   def numSquares(self, n: int) -> int:
-    ans = [n]
-    hs = set()
-
-    for i in range(int(sqrt(n)), 0, -1):
-      hs.add(i * i)
+    dp = [n] * (n + 1)
+    dp[0] = 0
 
     for i in range(1, n + 1):
-      if i in hs:
-        ans.append(1)
-        continue
+      for k in range(1, int(sqrt(n)) + 1):
+        if i - (k * k) < 0: continue
+        dp[i] = min(dp[i], dp[i - (k * k)] + 1)
 
-      mi = n
-      for k in hs:
-        if i - k < 0: continue
-        mi = min(mi, ans[i - k] + 1)
-      ans.append(mi)
-
-    return ans[n]
+    return dp[n]
