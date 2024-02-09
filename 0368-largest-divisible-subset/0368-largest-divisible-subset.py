63d20702a734d9ea3ffe5255d2489c6ec3655075
Luis Rodriguez <101071780+luisfrodriguezr@users.noreply.github.com>
2024-02-09 12:48:53 -0400

    Time: 300 ms (35.75%), Space: 17.5 MB (33.27%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -0,0 +1,30 @@
+class Solution:
+  def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+    nums.sort()
+    ans = list()
+    dp = [1] * len(nums)
+    hash_map = dict()
+    last = ma = 0
+    
+    def find(n):
+      if n == hash_map[n]:
+        ans.append(n)
+        return 
+      find(hash_map[n])
+      ans.append(n)
+      return 
+      
+    for i, num in enumerate(nums):
+      hash_map[num] = num
+      for j in range(0, i):
+        if nums[j] % num == 0 or num % nums[j] == 0:
+          if dp[j] + 1 > dp[i]:
+            dp[i] = dp[j] + 1
+            hash_map[num] = nums[j]
+      if dp[i] > ma:
+        ma = dp[i]
+        last = num
+    
+    find(last)
+    
+    return ans
