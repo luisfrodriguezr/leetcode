f0ef36ac62c4164cc6f3d3307a3db116da2a3881
Luis Rodriguez <101071780+luisfrodriguezr@users.noreply.github.com>
2024-04-25 09:50:39 -0400

    Time: 48 ms (37.80%) | Memory: 19.3 MB (28.97%) - LeetSync

---
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -0,0 +1,17 @@
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        diameter = 0
+
+        def longest_path(node):
+            if not node:
+                return 0
+            nonlocal diameter
+            left_path = longest_path(node.left)
+            right_path = longest_path(node.right)
+
+            diameter = max(diameter, left_path + right_path)
+
+            return max(left_path, right_path) + 1
+
+        longest_path(root)
+        return diameter 
