2945d985e89b1e851a095bc0e90ca6ab7039a56e
Luis Rodriguez <101071780+luisfrodriguezr@users.noreply.github.com>
2024-06-26 19:33:42 -0400

    Time: 253 ms (6.89%) | Memory: 20.4 MB (80.03%) - LeetSync

---
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.py
@@ -0,0 +1,49 @@
+class Solution:
+    def balanceBST(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+
+        vine_head = TreeNode(0)
+        vine_head.right = root
+        current = vine_head
+        while current.right:
+            if current.right.left:
+                self.right_rotate(current, current.right)
+            else:
+                current = current.right
+
+        node_count = 0
+        current = vine_head.right
+        while current:
+            node_count += 1
+            current = current.right
+
+        m = 2 ** math.floor(math.log2(node_count + 1)) - 1
+        self.make_rotations(vine_head, node_count - m)
+        while m > 1:
+            m //= 2
+            self.make_rotations(vine_head, m)
+
+        balanced_root = vine_head.right
+        # Delete the temporary dummy node
+        vine_head = None
+        return balanced_root
+
+    def right_rotate(self, parent: TreeNode, node: TreeNode):
+        tmp = node.left
+        node.left = tmp.right
+        tmp.right = node
+        parent.right = tmp
+
+    def left_rotate(self, parent: TreeNode, node: TreeNode):
+        tmp = node.right
+        node.right = tmp.left
+        tmp.left = node
+        parent.right = tmp
+
+    def make_rotations(self, vine_head: TreeNode, count: int):
+        current = vine_head
+        for _ in range(count):
+            tmp = current.right
+            self.left_rotate(current, tmp)
+            current = current.right 
