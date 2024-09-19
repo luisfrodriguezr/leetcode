6fe55f9d4664dd41f03ffb4388fbb21e889f5b58
Luis Rodriguez <101071780+luisfrodriguezr@users.noreply.github.com>
2024-09-19 14:13:52 -0400

    Time: 31 ms (77.93%) | Memory: 16.5 MB (24.52%) - LeetSync

--- 1660-thousand-separator/thousand-separator.py
+++ 1660-thousand-separator/thousand-separator.py
@@ -3,13 +3,10 @@ class Solution:
     l = list(str(n))
     ans = list()
 
-    aux = 0
-
     for i, c in enumerate(l[::-1]):
-      if aux % 3 == 0 and aux > 0:
+      if i % 3 == 0 and i > 0:
         ans.append('.')
       ans.append(c)
-      aux += 1
 
     ans.reverse()
     return ''.join(ans)
